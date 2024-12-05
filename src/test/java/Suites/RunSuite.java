cdb9a0e5e683c4b0444141ca02cb0450a39c95a1
DioneSousa <dione.msousa@yahoo.com.br>
2024-12-04 22:32:15 -0300

    adicionando pipe

---
+++ src/test/java/Suites/RunSuite.java
@@ -0,0 +1,11 @@
+package Suites;
+
+import GetUsuario.GetUsuarios;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({GetUsuarios.class})
+public class RunSuite {
+
+}
