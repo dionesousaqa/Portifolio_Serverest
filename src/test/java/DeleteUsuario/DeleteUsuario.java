8b46231b1cc895c5176ff00177b4f4a8bb8d1f19
DioneSousa <dione.msousa@yahoo.com.br>
2024-12-09 20:24:43 -0300

    Regatoração dos Testes de Delete

--- src/test/java/DeleteUsuario/DeleteUsuario.java
+++ src/test/java/DeleteUsuario/DeleteUsuario.java
@@ -5,12 +5,13 @@ import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import static Utils.TestesUtils.deletUsuario;
 import static Utils.TestesUtils.postUsuario;
 
 public class DeleteUsuario extends BaseTest {
     @Test
     public void deletandoUsuario () {
-        String id = postUsuario("AntonioPereira7650@hotmail.com");
+        String id = postUsuario("AntonioPereira22331@hotmail.com");
 
         RestAssured.given()
                    .when()
@@ -19,6 +20,7 @@ public class DeleteUsuario extends BaseTest {
                       .statusCode(200).log().all()
                       .body("message", Matchers.is("Registro excluído com sucesso"))
                 ;
+        deletUsuario(id);
     }
     @Test
     public void deletandoUsuarioComIdInexistente () {
