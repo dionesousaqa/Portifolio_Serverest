7b188651a494a99a775ed89a8ce19f0e1b727e0e
DioneSousa <dione.msousa@yahoo.com.br>
2024-12-09 20:28:38 -0300

    Adicionando nova feature de Put

--- src/test/java/Suites/RunSuite.java
+++ src/test/java/Suites/RunSuite.java
@@ -3,11 +3,12 @@ package Suites;
 import DeleteUsuario.DeleteUsuario;
 import GetUsuario.GetUsuarios;
 import PostUsuarios.PostUsuarios;
+import PutUsuario.PutUsuario;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({GetUsuarios.class, PostUsuarios.class, DeleteUsuario.class})
+@Suite.SuiteClasses({GetUsuarios.class, PostUsuarios.class, DeleteUsuario.class, PutUsuario.class})
 public class RunSuite {
 
 }
