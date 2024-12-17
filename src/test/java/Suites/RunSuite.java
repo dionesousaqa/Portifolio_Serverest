8eed222f3e7d919ac4dec0fdb7f28c7430c1f12d
DioneSousa <dione.msousa@yahoo.com.br>
2024-12-16 22:29:52 -0300

    Adicionei o teste E2E na pipe  no siute

--- src/test/java/Suites/RunSuite.java
+++ src/test/java/Suites/RunSuite.java
@@ -1,6 +1,7 @@
 package Suites;
 
 import DeleteUsuario.DeleteUsuario;
+import E2E.EndToEnd;
 import GetUsuario.GetUsuarios;
 import PostUsuarios.PostUsuarios;
 import PutUsuario.PutUsuario;
@@ -8,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({GetUsuarios.class, PostUsuarios.class, DeleteUsuario.class, PutUsuario.class})
+@Suite.SuiteClasses({GetUsuarios.class, PostUsuarios.class, DeleteUsuario.class, PutUsuario.class, EndToEnd.class})
 public class RunSuite {
 
 }
