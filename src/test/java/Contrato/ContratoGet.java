08593e442b003df5d934db9721a4e433966d9688
DioneSousa <dione.msousa@yahoo.com.br>
2024-12-31 16:13:02 -0300

    Adicionando Testes de Contrato

---
+++ src/test/java/Contrato/ContratoGet.java
@@ -0,0 +1,27 @@
+package Contrato;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import java.io.File;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+
+public class ContratoGet {
+    @Test
+    public void ValidacaoDeContrato(){
+        File jsonSchema = new File("src/test/resources/schemas/ContratoGet.json");
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("https://serverest.dev/usuarios")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchema(jsonSchema));
+
+
+                ;
+    }
+}
